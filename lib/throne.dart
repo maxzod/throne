738fc05fa5c432700f75e5def9fcc0a9833bacc3
maxzod <maxzod66@gmail.com>
2021-01-10 23:53:39 +0200

    fix imports

--- lib/throne.dart
+++ lib/throne.dart
@@ -2,4 +2,5 @@ library throne;
 
 export 'package:throne/queen_helper.dart';
 export 'package:throne/queen_throne.dart';
+export 'package:throne/queen_ship.dart';
 export 'package:throne/loyal_helpers/share_prefrancess.dart';
